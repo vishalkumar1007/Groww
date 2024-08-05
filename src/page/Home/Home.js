1598503f5157208addf876ff3f6fdde716094c63
vishalkumar1007 <vishalkumarnke93@gmail.com>
2024-08-05 06:24:50 +0530

    home page done 50%

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,8 +1,13 @@
 import './Home.css';
 import GrowwLogo from '../../assets/svg/groww-logo-light.svg';
 import Home_img from '../../assets/img/home_image_intro.png'
+import Mobile_image from '../../assets/img/stocksBuy.5382418f.webp'
+import indian_market_building from '../../assets/img/indianMarketBuilding.a399b6f2.webp'
+import Stocks_icon from '../../assets/img/stocksHistogram.1c5dd346.webp'
+import Mutual_icon from '../../assets/img/mutualFundBlocks.ee53101c.webp'
+import Future_icon from '../../assets/img/fnoClock.0c7a0775.webp'
 import { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
     const navigate = useNavigate();
     const [menuActive, setMenuActive] = useState(false);
@@ -20,7 +25,7 @@ const Home = () => {
                         <input type="text" placeholder='What are you looking for today' />
                     </div>
                     <div className="Home_nav_register" >
-                        <button onClick={()=>{navigate('login')}}>Login/Register</button>
+                        <button onClick={() => { navigate('login') }}>Login/Register</button>
                     </div>
                     <div className='Home_three_line_nav' onClick={() => { setMenuActive(!menuActive) }}>
                         {
@@ -34,7 +39,7 @@ const Home = () => {
                         menuActive ?
                             <div className='Home_three_menu_bar'>
                                 <div className="Home_nav_register_menu">
-                                    <button onClick={()=>{navigate('login')}}>Login/Register</button>
+                                    <button onClick={() => { navigate('login') }}>Login/Register</button>
                                 </div>
                             </div>
                             :
@@ -53,7 +58,7 @@ const Home = () => {
                                 <p>Build for growing India</p>
                             </div>
                             <div className='Home_body_section_1_text_btn'>
-                                <button>Get Started</button>
+                                <button onClick={() => { navigate('login') }}>Get Started</button>
                             </div>
                         </div>
                         <div className='Home_body_section_1_image'>
@@ -62,7 +67,91 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='Home_body_section_2'></div>
+                    <div className='Home_body_section_2'>
+                        <div className='Home_body_section_2_pd_horizontal'>
+                            <div className='Home_body_section_2_left'>
+                                <div className='Home_body_section_2_left_contain'>
+                                    <div className='Home_body_section_2_left_contain_icon_div'>
+                                        <div className='Home_body_section_2_left_contain_icon_div_img'>
+                                            <img src={indian_market_building} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className='Home_body_section_2_left_contain_title_div'>
+                                        <div className='Home_body_section_2_left_contain_title'>
+                                            <p>Indian markets at </p>
+                                            <p>your fingertips.</p>
+                                        </div>
+                                    </div>
+                                    <div className='Home_body_section_2_left_contain_tagline_div'>
+                                        <div className='Home_body_section_2_left_contain_tagline'>
+                                            <p>Long-term or short-term, high risk or low risk. Be the kind of investor you want to be.</p>
+                                        </div>
+                                    </div>
+                                    <div className='Home_body_section_2_left_contain_stock_option_div'>
+                                        <div className='Home_body_section_2_left_contain_stock_option_div_arrange'>
+                                            <div className='Home_body_section_2_left_contain_stock_option_1'>
+                                                <div className='Home_body_section_2_left_contain_stock_option_1_div'>
+                                                    <button className='Home_body_section_2_left_contain_stock_option_1_div_btn'>
+                                                        <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_arrange'>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_icon'>
+                                                                <img src={Stocks_icon} alt="" />
+                                                            </div>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_title'>
+                                                                <p>Stocks & Intraday</p>
+                                                            </div>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_arrow'>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6" /></svg>
+                                                            </div>
+                                                        </div>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <div className='Home_body_section_2_left_contain_stock_option_2'>
+                                                <div className='Home_body_section_2_left_contain_stock_option_1_div'>
+                                                    <button className='Home_body_section_2_left_contain_stock_option_1_div_btn'>
+                                                        <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_arrange'>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_icon'>
+                                                                <img src={Mutual_icon} alt="" />
+                                                            </div>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_title'>
+                                                                <p>Stocks & Intraday</p>
+                                                            </div>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_arrow'>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6" /></svg>
+                                                            </div>
+                                                        </div>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <div className='Home_body_section_2_left_contain_stock_option_3'>
+                                                <div className='Home_body_section_2_left_contain_stock_option_1_div'>
+                                                    <button className='Home_body_section_2_left_contain_stock_option_1_div_btn'>
+                                                        <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_arrange'>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_icon'>
+                                                                <img src={Future_icon} alt="" />
+                                                            </div>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_title'>
+                                                                <p>Stocks & Intraday</p>
+                                                            </div>
+                                                            <div className='Home_body_section_2_left_contain_stock_option_1_div_btn_arrow'>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6" /></svg>
+                                                            </div>
+                                                        </div>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='Home_body_section_2_right'>
+                                <div className='Home_body_section_2_right_mobile_div'>
+                                    <img src={Mobile_image} alt="" />
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                     <div className='Home_body_section_3'></div>
                     <div className='Home_body_section_4'></div>
                     <div className='Home_body_section_5'></div>
