44e572de63be7a87b44097d5e7ea8aea9463dc1d
vishalkumar1007 <vishalkumarnke93@gmail.com>
2024-10-24 05:24:57 +0530

    fix issue , logout popup not view properly in profile page but i fixed with some changes

--- src/page/Profile/Profile.jsx
+++ src/page/Profile/Profile.jsx
@@ -17,105 +17,103 @@ const Profile = () => {
   };
 
   useEffect(() => {
-    console.log(userProfileData);
-  }, [userProfileData]);
-
-  useEffect(() => {
     setUserName(userProfileData.userFirstName);
     setUserLastName(userProfileData.userLastName);
   }, [userProfileData]);
 
   return (
-    <div className="user_profile_main">
+    <>
       <Navbar />
       <div className="user_profile_main_arrange_width">
-        <div className="user_profile_left_container">
-          <div className="user_profile_left_image_box_main">
-            <div
-              className="user_profile_left_image_circle"
-              style={{ backgroundColor: `${userProfileData?.userColorCode}` }}
-            >
-              {userFirstName[0]}
-            </div>
-            <div className="user_profile_left_image_user_name">
-              <span>{`${userFirstName} ${userLaseName}`}</span>
-            </div>
-          </div>
-          <div className="user_profile_left_option_box_main">
-            <div
-              className="user_profile_left_option_box_main_basic_detail_section"
-              onClick={() => {
-                handelProfileActiveFeatureSection("basicDetail");
-              }}
-              style={{
-                backgroundColor:
-                  profileViewFeatureSection === "basicDetail"
-                    ? "#f0f0f0ea"
-                    : null,
-              }}
-            >
-              <div className="user_profile_left_option_box_main_section_heading">
-                <span>Basic Details</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#515151"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="m9 18 6-6-6-6" />
-                </svg>
+        <div className="user_profile_main_make_center_contain_box">
+          <div className="user_profile_left_container">
+            <div className="user_profile_left_image_box_main">
+              <div
+                className="user_profile_left_image_circle"
+                style={{ backgroundColor: `${userProfileData?.userColorCode}` }}
+              >
+                {userFirstName[0]}
+              </div>
+              <div className="user_profile_left_image_user_name">
+                <span>{`${userFirstName} ${userLaseName}`}</span>
               </div>
             </div>
-            <div
-              className="user_profile_left_option_box_main_edit_profile_section"
-              onClick={() => {
-                handelProfileActiveFeatureSection("updateProfile");
-              }}
-              style={{
-                backgroundColor:
-                  profileViewFeatureSection === "updateProfile"
-                    ? "#f0f0f0ea"
-                    : null,
-              }}
-            >
-              <div className="user_profile_left_option_box_main_section_heading">
-                <span>Update Profile</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#515151"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="m9 18 6-6-6-6" />
-                </svg>
+            <div className="user_profile_left_option_box_main">
+              <div
+                className="user_profile_left_option_box_main_basic_detail_section"
+                onClick={() => {
+                  handelProfileActiveFeatureSection("basicDetail");
+                }}
+                style={{
+                  backgroundColor:
+                    profileViewFeatureSection === "basicDetail"
+                      ? "#f0f0f0ea"
+                      : null,
+                }}
+              >
+                <div className="user_profile_left_option_box_main_section_heading">
+                  <span>Basic Details</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#515151"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="m9 18 6-6-6-6" />
+                  </svg>
+                </div>
+              </div>
+              <div
+                className="user_profile_left_option_box_main_edit_profile_section"
+                onClick={() => {
+                  handelProfileActiveFeatureSection("updateProfile");
+                }}
+                style={{
+                  backgroundColor:
+                    profileViewFeatureSection === "updateProfile"
+                      ? "#f0f0f0ea"
+                      : null,
+                }}
+              >
+                <div className="user_profile_left_option_box_main_section_heading">
+                  <span>Update Profile</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#515151"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="m9 18 6-6-6-6" />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="user_profile_right_container">
-          {profileViewFeatureSection === "basicDetail" ? (
-            <BasicDetail userProfileData={userProfileData}/>
-          ) : (
-            <UpdateProfile userProfileData={userProfileData} />
-          )}
+          <div className="user_profile_right_container">
+            {profileViewFeatureSection === "basicDetail" ? (
+              <BasicDetail userProfileData={userProfileData} />
+            ) : (
+              <UpdateProfile userProfileData={userProfileData} />
+            )}
+          </div>
         </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
-const BasicDetail = ({userProfileData=""}) => {
+const BasicDetail = ({ userProfileData = "" }) => {
   return (
     <div className="user_profile_basic_detail_component_main">
       <div className="user_profile_basic_detail_component_main_design_1"></div>
@@ -134,15 +132,13 @@ const BasicDetail = ({userProfileData=""}) => {
               FIRST NAME
             </label>
             <div id="basicDetail_name_showData">
-                {
-                    userProfileData
-                    ? `${
-                        userProfileData.userFirstName
-                          ? userProfileData.userFirstName
-                          : "update now"
-                      }`
-                    : "user first name"
-                }
+              {userProfileData
+                ? `${
+                    userProfileData.userFirstName
+                      ? userProfileData.userFirstName
+                      : "update now"
+                  }`
+                : "user first name"}
             </div>
           </div>
           <div
@@ -153,15 +149,13 @@ const BasicDetail = ({userProfileData=""}) => {
               LAST NAME
             </label>
             <div id="basicDetail_name_showData">
-                {
-                    userProfileData
-                    ? `${
-                        userProfileData.userLastName
-                          ? userProfileData.userLastName
-                          : "update now"
-                      }`
-                    : "user last name"
-                }
+              {userProfileData
+                ? `${
+                    userProfileData.userLastName
+                      ? userProfileData.userLastName
+                      : "update now"
+                  }`
+                : "user last name"}
             </div>
           </div>
           <div
@@ -172,15 +166,13 @@ const BasicDetail = ({userProfileData=""}) => {
               EMAIL
             </label>
             <div id="basicDetail_name_showData">
-                {
-                    userProfileData
-                    ? `${
-                        userProfileData.userEmail
-                          ? userProfileData.userEmail
-                          : "update now"
-                      }`
-                    : "user@gmail.com"
-                }
+              {userProfileData
+                ? `${
+                    userProfileData.userEmail
+                      ? userProfileData.userEmail
+                      : "update now"
+                  }`
+                : "user@gmail.com"}
             </div>
           </div>
           <div
@@ -191,15 +183,13 @@ const BasicDetail = ({userProfileData=""}) => {
               MOBILE NUMBER
             </label>
             <div id="basicDetail_name_showData">
-                {
-                     userProfileData
-                     ? `${
-                         userProfileData.userMobileNumber
-                           ? userProfileData.userMobileNumber
-                           : "update now"
-                       }`
-                     : "+91 6205594943"
-                }
+              {userProfileData
+                ? `${
+                    userProfileData.userMobileNumber
+                      ? userProfileData.userMobileNumber
+                      : "update now"
+                  }`
+                : "+91 6205594943"}
             </div>
           </div>
         </div>
@@ -212,15 +202,13 @@ const BasicDetail = ({userProfileData=""}) => {
               DATE OF BIRTH
             </label>
             <div id="basicDetail_name_showData">
-                {
-                    userProfileData
-                    ? `${
-                        userProfileData.userDateOfBirth
-                          ? userProfileData.userDateOfBirth
-                          : "update now"
-                      }`
-                    : "17 July 2003"
-                }
+              {userProfileData
+                ? `${
+                    userProfileData.userDateOfBirth
+                      ? userProfileData.userDateOfBirth
+                      : "update now"
+                  }`
+                : "17 July 2003"}
             </div>
           </div>
           <div
@@ -231,15 +219,13 @@ const BasicDetail = ({userProfileData=""}) => {
               GENDER
             </label>
             <div id="basicDetail_name_showData">
-                {
-                    userProfileData
-                    ? `${
-                        userProfileData.userGender
-                          ? userProfileData.userGender
-                          : "update now"
-                      }`
-                    : "Male"
-                }
+              {userProfileData
+                ? `${
+                    userProfileData.userGender
+                      ? userProfileData.userGender
+                      : "update now"
+                  }`
+                : "Male"}
             </div>
           </div>
           <div
@@ -250,15 +236,13 @@ const BasicDetail = ({userProfileData=""}) => {
               MARITAL STATUS
             </label>
             <div id="basicDetail_name_showData">
-                {
-                    userProfileData
-                    ? `${
-                        userProfileData.userMaritalStatus
-                          ? userProfileData.userMaritalStatus
-                          : "update now"
-                      }`
-                    : "Single"
-                }
+              {userProfileData
+                ? `${
+                    userProfileData.userMaritalStatus
+                      ? userProfileData.userMaritalStatus
+                      : "update now"
+                  }`
+                : "Single"}
             </div>
           </div>
           <div
@@ -268,7 +252,17 @@ const BasicDetail = ({userProfileData=""}) => {
             <label htmlFor="basicDetail_name_main" id="basicDetail_name_lable">
               PROFILE COLOR CODE
             </label>
-            <div id="basicDetail_name_showData">#a5174ff5</div>
+            <div id="basicDetail_name_showData">
+              {
+                userProfileData
+                ? `${
+                    userProfileData.userColorCode
+                      ? userProfileData.userColorCode
+                      : "update now"
+                  }`
+                : "#000000"
+              }
+            </div>
           </div>
         </div>
       </div>
